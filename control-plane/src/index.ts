35bbe8e3255b325e75ba2cf67399d81a454de0c8
Troy Ciesco <tmciesco@gmail.com>
2025-01-30 15:48:44 -0500

    change cp to bun for now, add ws

--- control-plane/src/index.ts
+++ control-plane/src/index.ts
@@ -1,11 +1,12 @@
-import { serve } from "@hono/node-server"
 import { Hono } from "hono"
 import { cors } from "hono/cors"
 import { dungeons } from "./routes/dungeons"
 import { adventurers } from "./routes/adventurers"
 import { parties } from "./routes/parties"
+import { createBunWebSocket } from "hono/bun"
 
 const app = new Hono()
+const { upgradeWebSocket, websocket } = createBunWebSocket<any>()
 
 app.use("/*", cors())
 
@@ -17,10 +18,29 @@ app.route("/", dungeons)
 app.route("/", adventurers)
 app.route("/", parties)
 
+app.get(
+	"/ws",
+	upgradeWebSocket((c) => {
+		let intervalId: any
+
+		return {
+			onOpen(_event, ws) {
+				intervalId = setInterval(() => {
+					ws.send(new Date().toString())
+				}, 1000)
+			},
+			onClose() {
+				clearInterval(intervalId)
+			}
+		}
+	})
+)
+
 const port = 1111
 console.log(`Server is running on http://localhost:${port}`)
 
-serve({
+export default {
 	fetch: app.fetch,
-	port
-})
+	port,
+	websocket
+}
--- ui/src/routes/home.tsx
+++ ui/src/routes/home.tsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react"
 import { Link } from "react-router"
 
 export function Home() {
+	const [messages, setMessages] = useState<string[]>([])
+	// info about double messages https://github.com/facebook/create-react-app/issues/10387
+	useEffect(() => {
+		const ws = new WebSocket("ws://localhost:1111/ws")
+		ws.onopen = () => {
+			console.log("connected")
+		}
+		ws.onmessage = (event) => {
+			setMessages((m) => [...m, event.data as string])
+		}
+
+		return () => {
+			ws.close()
+		}
+	}, [])
+
 	return (
 		<div className="bg-red-400">
 			<h1 className="text-7xl">hello world</h1>
 			<Link to="/dungeons">Go to dungeons</Link>
+			{messages.map((message) => (
+				<div key={message}>{message}</div>
+			))}
 		</div>
 	)
 }
