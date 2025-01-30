1608ce8ea24f05d556e439eefcb0d3e9a19db794
Troy Ciesco <tmciesco@gmail.com>
2025-01-30 13:15:43 -0500

    test out different validators and form libraries

--- ui/src/routes/adventurers/create.tsx
+++ ui/src/routes/adventurers/create.tsx
@@ -1,7 +1,61 @@
+import { useForm } from "react-hook-form"
+import { arktypeResolver } from "@hookform/resolvers/arktype"
+import { type } from "arktype"
+
+const adventurerSpecies = [
+	"Dwarf",
+	"Elf",
+	"Halfling",
+	"Human",
+	"Dragonborn",
+	"Gnome",
+	"Half-Elf",
+	"Half-Orc",
+	"Tiefling"
+] as const
+
+const schema = type({
+	name: type.string.moreThanLength(7).describe("at least seven chars fam"),
+	species: type.enumerated([...adventurerSpecies]).describe("a valid species"),
+	class: "string>1"
+})
+
 export function AdventurerCreateRoute() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm({
+		resolver: arktypeResolver(schema)
+	})
+
 	return (
 		<div>
 			<h1>Create an Adventurer</h1>
+			<form onSubmit={handleSubmit((d) => console.log(d))}>
+				<div>
+					<label>
+						Name
+						<input className="border" {...register("name")} />
+					</label>
+					<p>{errors.name?.message?.toString()}</p>
+				</div>
+				<div>
+					<label>
+						Species
+						<input className="border" {...register("species")} />
+					</label>
+					<p>{errors.species?.message?.toString()}</p>
+				</div>
+				<div>
+					<label>
+						Class
+						<input className="border" {...register("class")} />
+					</label>
+					<p>{errors.class?.message?.toString()}</p>
+				</div>
+				<button type="submit">Submit</button>
+			</form>
 		</div>
 	)
 }
--- ui/src/routes/dungeons/create.tsx
+++ ui/src/routes/dungeons/create.tsx
@@ -1,7 +1,36 @@
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from "zod"
+
+const schema = z.object({
+	name: z.string().min(1, { message: "A name for the dungeon is required" }),
+	// location: z.string().min(1, { message: "A location is required" })
+	location: z.enum(["Alpha", "Bravo", "Charlie"], {
+		message: "gotta be alpha or bravo or charlie"
+	})
+})
+
 export function DungeonCreateRoute() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm({
+		resolver: zodResolver(schema)
+	})
+
 	return (
-		<div>
-			<h1>Create a Dungeon</h1>
-		</div>
+		<>
+			<div>Create a dungeon</div>
+			<form onSubmit={handleSubmit((d) => console.log(d))}>
+				<input className="border" {...register("name")} />
+				{errors.name?.message && <p>{errors.name?.message.toString()}</p>}
+				<input className="border" {...register("location")} />
+				{errors.location?.message && (
+					<p>{errors.location?.message.toString()}</p>
+				)}
+				<button>Submit</button>
+			</form>
+		</>
 	)
 }
